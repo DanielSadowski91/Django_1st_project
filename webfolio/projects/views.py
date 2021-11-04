15e0c47b544e7d4324103f38bbef3c77f44cdf4b
Daniel <d.sadowski91@gmail.com>
2021-11-04 09:00:07 +0100

    menu

--- webfolio/projects/views.py
+++ webfolio/projects/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        'id': '1',
+        'title': "E-commerce Website",
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': 'Project with portfolio'
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': 'People with i work'
+    },
+]
+
 
 def projects(request):
-    msg = 'Hello, you are on the projects page'
-    return render(request, 'projects/projects.html', {'message': msg})
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
