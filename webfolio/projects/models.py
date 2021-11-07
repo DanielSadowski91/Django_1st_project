993ea08ac6cb193ac86ecd4d347aa7f2fb280e54
Daniel <d.sadowski91@gmail.com>
2021-11-07 13:57:50 +0100

    class tag

--- webfolio/projects/models.py
+++ webfolio/projects/models.py
@@ -29,3 +29,16 @@ class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=2000)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
